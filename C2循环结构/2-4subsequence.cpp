b7918a6b3f2b63c6d283887e3c24cbc8e4549495
siluweston <64230000+siluweston@users.noreply.github.com>
2021-07-26 13:27:25 -0400

    Update and rename 2-5subsequence.cpp to 2-4subsequence.cpp

--- C2循环结构/2-4subsequence.cpp
+++ C2循环结构/2-4subsequence.cpp
@@ -1,3 +1,13 @@
+/*
+Given positive integers n<m<1e6, output 1/(n^2) + 1/((n+1)^2) + ... + 1/(m^2), keeping 5 decimal places. Terminate when n=m=0.
+Input:
+2 4
+65536 655360
+0 0 
+Output:
+Case 1: 0.42361
+Case 2: 0.00001
+*/
 #include<iostream>
 using namespace std;
 
